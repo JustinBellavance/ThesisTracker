be016389483f63944b8fd19231cbd76db50c97b5
Justin <justin.bellavance@gmail.com>
2025-02-17 22:57:24 -0500

    improved security and added reset password flow

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 
 from . import views
@@ -11,4 +12,8 @@ urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
     path("profile/", views.profile, name="profile"),
     path('load-contribution-calendars', views.load_contribution_calendars, name='load_contribution_calendars'),
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
